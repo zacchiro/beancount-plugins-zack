182394b1c2152f9e245bad222a9cde05f1bd89a4
Stefano Zacchiroli <zack@upsilon.cc>
2018-05-31 22:52:25 +0200

    no_missing_documents: add support to check document directives

--- no_missing_documents.py
+++ no_missing_documents.py
@@ -1,22 +1,31 @@
 # Copyright (C) 2018 Stefano Zacchiroli <zack@upsilon.cc>
 # License: GNU General Public License (GPL), version 2 or above
 
-"""This Beancount plugin validates that each Beancount transaction that
-contains a document metadata key has as value a path corresponding to an
-existing file on disk.
+"""This Beancount plugin validates that documents referenced from Beancount
+files correspond to existing files on disks. It will return a
+DocumentNotFoundError for each document that cannot be found on disk.
 
-The list of metadata keys considered to be documents default to the single key
-"document". The default can be overridden passing a configuration string, which
-should be a comma-separated list of metadata keys, e.g.::
+Given a list of Beancount entries, the following documents will be checked by
+the plugin:
 
-    plugin "no_missing_documents" "receipt,statement,invoice"
+* Filenames of ``document`` directives.
+
+* Metadata associated to any directive, as long as the metadata key is in the
+  (configurable) set of keys that the plugin will consider.
+
+  By default, the plugin will look only for the `document` metadata. The
+  default can be overridden passing the plugin a configuration string, which
+  should be a comma-separated list of metadata keys, e.g.::
+
+      plugin "no_missing_documents" "receipt,statement,invoice"
 
 """
 
 import collections
 import os
 
-from beancount.core.data import filter_txns
+from beancount.core import data
+
 
 __plugins__ = ('validate_documents',)
 
@@ -29,9 +38,23 @@ DocumentNotFoundError = collections.namedtuple(
 # default list of metada keys that are expected to point to existing documents
 DEFAULT_KEYS = ['document']
 
+CHECK_DOCUMENT_ENTRIES = False  # disabled as Beancount core does this already
+
 
 def parse_keys(config_str):
     return config_str.split(',')
+
+
+def check_missing(entry, document):
+    errors = []
+
+    if not os.path.isfile(document):
+        errors.append(DocumentNotFoundError(
+            entry.meta,
+            'Document not found: {}'.format(document),
+            entry))
+
+    return errors
 
 
 def validate_documents(entries, options_map, config_str=None):
@@ -52,15 +75,13 @@ def validate_documents(entries, options_map, config_str=None):
         document_keys = parse_keys(config_str)
 
     for entry in entries:
+        # check the filename property of document directives
+        if isinstance(entry, data.Document) and CHECK_DOCUMENT_ENTRIES:
+            errors.extend(check_missing(entry, entry.filename))
+
+        # check requested metadata of all directives (including documents)
         for key in document_keys:
-            try:
-                document = entry.meta[key]
-                if not os.path.isfile(document):
-                    errors.append(DocumentNotFoundError(
-                        entry.meta,
-                        'Document not found: {}'.format(document),
-                        entry))
-            except KeyError:
-                pass
+            if key in entry.meta:
+                errors.extend(check_missing(entry, entry.meta[key]))
                 
     return entries, errors
